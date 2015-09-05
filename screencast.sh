01feb3274ef5d784ec41d833819bca4a5d0f40a9
Andrej Zakharevich <andrej@zahar.ws>
2015-09-05 21:34:06 +0300

    Add nice command to renice screencast capturing As niceness is set to 19 the screencast capturing screept will has less favorable status during process scheduling.

--- screencast.sh
+++ screencast.sh
@@ -2,4 +2,4 @@
 
 format=$(xwininfo -root | grep 'geometry' | awk '{print $2}' | awk -F+ '{print $1}')
 
-ffmpeg  -f x11grab -r 8 -s $format -i :0.0 -an -vcodec libx264 -vsync 1 -y $(date "+%F_%H.%M.%S").avi
+nice -n 19 ffmpeg  -f x11grab -r 8 -s $format -i :0.0 -an -vcodec libx264 -vsync 1 -y $(date "+%F_%H.%M.%S").avi
